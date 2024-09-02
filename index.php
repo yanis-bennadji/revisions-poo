198d30744d0794a590389f8caa98202717c01a3d
Yanis Bennadij <yanis.bennadji@laplateforme.io>
2024-09-02 09:28:37 +0200

    First commit

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Revision - POO</title>
+</head>
+<body>
+    <main>
+        <h1>
+            Jobs : 
+        </h1>
+
+        <ul>
+            <li>
+                <a href="./job01/index.php">Job 01</a>
+            </li>
+        </ul>
+    </main>
+    
+</body>
+</html>
